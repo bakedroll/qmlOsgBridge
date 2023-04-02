8d51238d65f565d241dd25d1cd98ae9842d0ecb7
Daniel Borchart <daniel.borchart@protonmail.com>
2023-04-02 20:22:49 +0200

    first commit

---
+++ qmlOsgBridgeApplication/resources/mainwindow.qml
@@ -0,0 +1,12 @@
+import QtQuick 2.15
+import QtQuick.Window 2.15
+
+Window {
+    width: 640
+    height: 480
+    minimumWidth: 300
+    minimumHeight: 300
+    title: "qmlOsgBridgeApplication"
+    visible: true
+
+}
---
+++ qmlOsgBridgeApplication/src/main.cpp
@@ -0,0 +1,16 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QQuickStyle>
+
+int main(int argc, char** argv)
+{
+  //QCoreApplication::setAttribute(Qt::AA_UseSoftwareOpenGL);
+  QGuiApplication app(argc, argv);
+
+  QQuickStyle::setStyle("Fusion");
+
+  QQmlApplicationEngine engine;
+  engine.load(QUrl(QStringLiteral("qrc:/resources/mainwindow.qml")));
+
+  return app.exec();
+}
